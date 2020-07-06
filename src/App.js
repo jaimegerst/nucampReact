cb57646e5747ee2e184d2cc8f668abac1bfbb76e
Jaime Gerst <jaimegerst1@gmail.com>
2020-07-06 17:06:45 -0400

    Functional Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import Main from './components/MainComponent';
-=======
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Directory from './components/DirectoryComponents';
->>>>>>> 559a3351440b972c7909f60aaf7d58b62a536532
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-<<<<<<< HEAD
                 <Main />
-=======
-                <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">NuCamp</NavbarBrand>
-                </div>
-                </Navbar>
-                <Directory />
->>>>>>> 559a3351440b972c7909f60aaf7d58b62a536532
             </div>
         );
     };
--- src/components/DirectoryComponents.js
+++ src/components/DirectoryComponents.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
-<<<<<<< HEAD
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-class Directory extends Component {
+function RenderDirectoryItem({campsite, onClick}) {
+    return (
+        <Card onClick={() => onClick(campsite.id)}>
+            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+            <CardImgOverlay>
+                <CardTitle>{campsite.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    )
+}
 
-    render() {
-        const directory = this.props.campsites.map(campsite => {
-            return (
-                <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.props.onClick(campsite.id)}>
-                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{campsite.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-            );
-        });
+function Directory(props) {
+
+    const directory = props.campsites.map(campsite => {
+        return (
+            <div key={campsite.id} className="col-md-5 m-1">
+                <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
+            </div>
+        );
+    });
 
         return (
             <div className="container">
@@ -26,65 +30,5 @@ class Directory extends Component {
             </div>
         );
     }
-}
-=======
-
-class Directory extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: [
-                {
-                    id: 0,
-                    name: 'React Lake Campground',
-                    image: 'assets/images/react-lake.jpg',
-                    elevation: 1233,
-                    description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers."
-                },
-                {
-                  id: 1,
-                  name: 'Chrome River Campground ',
-                  image: 'assets/images/chrome-river.jpg',
-                  elevation: 877,
-                  description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River."
-                },
-                {
-                    id: 2,
-                    name: 'Breadcrumb Trail Campground',
-                    image: 'assets/images/breadcrumb-trail.jpg',
-                    elevation: 2901,
-                    description: "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground."
-                },
-                {
-                    id: 3,
-                    name: 'Redux Woods Campground',
-                    image: 'assets/images/redux-woods.jpg',
-                    elevation: 42,
-                    description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
-                }
-            ],
-        };
-    }
-        render() {
-            const directory = this.state.campsites.map(campsite => {
-                return (
-                    <div key={campsite.id} className="col">
-                        <img src={campsite.image} alt={campsite.name} />
-                        <h2>{campsite.name}</h2>
-                        <p>{campsite.description}</p>
-                    </div>
-                );
-            });
-    
-            return (
-                <div className="container">
-                    <div className="row">
-                        {directory}
-                    </div>
-                </div>
-            );
-        }
-    }
->>>>>>> 559a3351440b972c7909f60aaf7d58b62a536532
 
 export default Directory;
